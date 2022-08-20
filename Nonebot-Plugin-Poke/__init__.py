ac4b77f6bebf5c33874ffbde5927f60523a44aef
BuDing OwO <ConBlueCut@outlook.com>
2022-08-21 05:25:34 +0800

    Update

--- Nonebot-Plugin-Poke/__init__.py
+++ Nonebot-Plugin-Poke/__init__.py
@@ -7,8 +7,10 @@ from nonebot.rule import Rule
 from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11.message import Message, MessageSegment
 from nonebot.params import CommandArg, ArgPlainText, Arg
-from nonebot.adapters.onebot.v11 import MessageEvent, Event, Bot, GroupMessageEvent
+from nonebot.adapters.onebot.v11 import MessageEvent, Event, Bot
 from nonebot.params import State
+from typing import Union
+import json
 
 global_config = get_driver().config
 config = Config.parse_obj(global_config)
@@ -30,6 +32,27 @@ __list__ = """
 __usage__ = f"{__des__}\n\nUsage:\n{__cmd__}\n\nCommandList:[指令名 | 指令介绍]\n{__list__}"
 
 
+def get_at_user_id(data: str) -> Union[list[str], list[int], list]:
+    """
+    获取at里的qq，返回[qq, qq, qq,...]
+    全体成员直接返回['all']
+    如果没有at任何人，返回[]
+    :param data: event.json
+    :return: list
+    """
+    try:
+        qq_list = []
+        data = json.loads(data)
+        for msg in data["message"]:
+            if msg["type"] == "at":
+                if 'all' not in str(msg):
+                    qq_list.append(int(msg["data"]["qq"]))
+                else:
+                    return ['all']
+        return qq_list
+    except KeyError:
+        return []
+
 
 matcher = on_command("戳我", priority=5, block=False)
 
@@ -39,17 +62,15 @@ async def poke(event: Event, matcher: Matcher):
     user_id = event.get_user_id()
 
     at = MessageSegment.at(user_id=user_id)
-    # poke = MessageSegment.poke(type_="poke", id_=str(user_id))
-    poke = f'[CQ:poke,qq={user_id}]'
+    poke = MessageSegment.poke(type_="poke", id_=str(user_id))
+    # poke = f'[CQ:poke,qq={user_id}]'
     await atname.send(at + MessageSegment.face(28) + MessageSegment.face(109) +
-                       MessageSegment.face(183) + "rua死你！！！")
+                      MessageSegment.face(183) + "rua死你！！！")
     count = 0
     while count <= 8:
         await matcher.send(poke)
         count = count + 2
         time.sleep(1)
-
-    await matcher.finish(poke)
 
 
 ################
@@ -59,57 +80,22 @@ async def _checker_(bot: Bot, event: Event, state: T_State) -> bool:
     msg = str(event.get_message())
     return True if 'CQ:at' in msg else False
 
-# rule=Rule(_checker_)
-
 
 atname = on_command("戳", priority=5, rule=Rule(_checker_), block=False)
 
 
 @atname.handle()
-async def user_id_from_at(event: MessageEvent or Event, matcher: Matcher, state: T_State = State()):  # , args: Message = CommandArg()):
-    msg = str(event.get_message())
-    # msg = msg.lstrip(str({"".join(list(get_driver().config.command_start))}))
-    # msg = msg.replace("/", "")
-    # print(type({"".join(list(get_driver().config.command_start))}))
-    # msg = msg.lstrip("戳")
-    msg = msg.replace("[CQ:at,qq=", "").replace("]", "")
-    msg = msg[2:]
-    """plain_text = args.extract_plain_text()
-    if plain_text:
-        matcher.set_arg("msg", args)
+async def user_id_from_at(event: MessageEvent or Event, matcher: Matcher):
+    user_id = get_at_user_id(event.json())[0]
+    at = MessageSegment.at(user_id=user_id)
+    poke = MessageSegment.poke(type_="poke", id_=str(user_id))
+    # poke = f'[CQ:poke,qq={user_id}]'
 
-    if plain_text not in [""]:
-
-        Cache = plain_text
-        user_id = Cache"""
-    user_id = int(msg)
-
-
-    # raw_msg = _event_.original_message
-    # print(raw_msg)
-
-    # qq=str(raw_msg.data.get("qq", ""))
-
+    await matcher.send(at + MessageSegment.face(28) + MessageSegment.face(109) +
+                       MessageSegment.face(183) + "rua死你！！！")
 
     count = 0
-    at = MessageSegment.at(user_id=user_id)
-    # poke = MessageSegment.poke(type_="poke", id_=str(user_id))
-    poke = f'[CQ:poke,qq={user_id}]'
-
-    
-    await atname.send(at + MessageSegment.face(28) + MessageSegment.face(109) +
-                       MessageSegment.face(183) + "rua死你！！！")
-    _list_ = ["2821323220"]
-
-    if user_id in _list_:
-        while count <= 12:
-            await atname.send(poke)
-            count = count + 2
-        await atname.finish(poke)
-    if user_id not in _list_:
-        count = 0
-        while count <= 8:
-            await atname.send(poke)
-            count = count + 2
-            time.sleep(1)
-        await atname.finish(poke)
+    while count <= 8:
+        await matcher.send(poke)
+        count = count + 2
+        time.sleep(1)
